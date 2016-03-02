126f6e5a155fc53fc22528a9a23c9f775ced9137
Jason Pilz <jasonpilz@gmail.com>
2016-03-02 09:29:19 -0700

    set up socket.io

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const http = require('http');
+const http    = require('http');
 const express = require('express');
-
-const app = express();
+const app     = express();
 
 app.use(express.static('public'));
 
@@ -9,12 +8,32 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-const port = process.env.PORT || 3000;
-
+const port   = process.env.PORT || 3000;
 const server = http.createServer(app)
                  .listen(port, () => {
                    console.log('Listening on port ' + port + '.');
                  });
+
+const socketIo = require('socket.io');
+const io       = socketIo(server);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = server;
